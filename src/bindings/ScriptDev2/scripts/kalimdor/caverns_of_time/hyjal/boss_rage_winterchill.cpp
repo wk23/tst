2070cea8686621c5bfbfefa685611e9c71552fd1
unknown <Administrator@.(none)>
2010-03-07 22:55:04 +0200

    sd2 1

---
+++ src/bindings/ScriptDev2/scripts/kalimdor/caverns_of_time/hyjal/boss_rage_winterchill.cpp
@@ -0,0 +1,168 @@
+#include "precompiled.h"
+#include "def_hyjal.h"
+#include "hyjal_trash.h"
+
+#define SPELL_FROST_ARMOR 31256
+#define SPELL_DEATH_AND_DECAY 31258
+
+#define SPELL_FROST_NOVA 31250
+#define SPELL_ICEBOLT 31249
+
+#define SAY_ONDEATH "You have won this battle, but not... the... war"
+#define SOUND_ONDEATH 11026
+
+#define SAY_ONSLAY1 "All life must perish!"
+#define SAY_ONSLAY2 "Victory to the Legion!"
+#define SOUND_ONSLAY1 11025
+#define SOUND_ONSLAY2 11057
+
+#define SAY_DECAY1 "Crumble and rot!"
+#define SAY_DECAY2 "Ashes to ashes, dust to dust"
+#define SOUND_DECAY1 11023
+#define SOUND_DECAY2 11055
+
+#define SAY_NOVA1 "Succumb to the icy chill... of death!"
+#define SAY_NOVA2 "It will be much colder in your grave"
+#define SOUND_NOVA1 11024
+#define SOUND_NOVA2 11058
+
+#define SAY_ONAGGRO "The Legion's final conquest has begun! Once again the subjugation of this world is within our grasp. Let none survive!"
+#define SOUND_ONAGGRO 11022
+
+struct MANGOS_DLL_DECL boss_rage_winterchillAI : public ScriptedAI
+{
+    boss_rage_winterchillAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        pInstance = ((ScriptedInstance*)pCreature->GetInstanceData());
+        go = false;    
+        pos = 0;
+        Reset();
+    }
+
+    uint32 FrostArmorTimer;
+    uint32 DecayTimer;
+    uint32 NovaTimer;
+    uint32 IceboltTimer;
+    bool go;
+    uint32 pos;
+        ScriptedInstance* pInstance;
+        bool IsEvent;
+        uint32 Delay;
+        uint32 LastOverronPos;
+        bool IsOverrun;
+        bool SetupOverrun;
+        uint32 OverrunType;
+        uint8 faction;
+        bool useFlyPath;
+        uint32 damageTaken;
+        float DummyTarget[3];
+
+
+    void Reset()
+    {
+        FrostArmorTimer = 37000;
+        DecayTimer = 45000;
+        NovaTimer = 15000;
+        IceboltTimer = 10000;
+
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_RAGEWINTERCHILLEVENT, NOT_STARTED);
+    }
+
+    void Aggro(Unit *who)
+    {    
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_RAGEWINTERCHILLEVENT, IN_PROGRESS);
+        DoPlaySoundToSet(m_creature, SOUND_ONAGGRO);
+        m_creature->MonsterYell(SAY_ONAGGRO, LANG_UNIVERSAL, NULL);
+    }
+
+    void KilledUnit(Unit *victim)
+    {
+        switch(rand()%2)
+        {
+            case 0:
+                DoPlaySoundToSet(m_creature, SOUND_ONSLAY1);
+                m_creature->MonsterYell(SAY_ONSLAY1, LANG_UNIVERSAL, NULL);
+                break;
+            case 1:
+                DoPlaySoundToSet(m_creature, SOUND_ONSLAY2);
+                m_creature->MonsterYell(SAY_ONSLAY2, LANG_UNIVERSAL, NULL);
+                break;        
+        }        
+    }
+
+    void JustDied(Unit *victim)
+    {
+        if(pInstance && IsEvent)
+            pInstance->SetData(DATA_RAGEWINTERCHILLEVENT, DONE);
+        DoPlaySoundToSet(m_creature, SOUND_ONDEATH);
+        m_creature->MonsterYell(SAY_ONDEATH, LANG_UNIVERSAL, NULL);
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+        //Return since we have no target
+        if(!m_creature->SelectHostileTarget() || !m_creature->getVictim() )
+            return;
+
+        if(FrostArmorTimer < diff)
+        {
+            DoCast(m_creature, SPELL_FROST_ARMOR);
+            FrostArmorTimer = 40000+rand()%20000;
+        }else FrostArmorTimer -= diff;
+        if(DecayTimer < diff)
+        {
+            DoCast(m_creature->getVictim(), SPELL_DEATH_AND_DECAY);
+            DecayTimer = 60000+rand()%20000;
+            switch(rand()%2)
+            {
+                case 0:
+                    DoPlaySoundToSet(m_creature, SOUND_DECAY1);
+                    m_creature->MonsterYell(SAY_DECAY1, LANG_UNIVERSAL, NULL);
+                    break;
+                case 1:
+                    DoPlaySoundToSet(m_creature, SOUND_DECAY2);
+                    m_creature->MonsterYell(SAY_DECAY2, LANG_UNIVERSAL, NULL);
+                    break;        
+            }    
+        }else DecayTimer -= diff;
+        if(NovaTimer < diff)
+        {
+            DoCast(m_creature->getVictim(), SPELL_FROST_NOVA);
+            NovaTimer = 30000+rand()%15000;
+            switch(rand()%2)
+            {
+                case 0:
+                    DoPlaySoundToSet(m_creature, SOUND_NOVA1);
+                    m_creature->MonsterYell(SAY_NOVA1, LANG_UNIVERSAL, NULL);
+                    break;
+                case 1:
+                    DoPlaySoundToSet(m_creature, SOUND_NOVA2);
+                    m_creature->MonsterYell(SAY_NOVA2, LANG_UNIVERSAL, NULL);
+                    break;        
+            }
+        }else NovaTimer -= diff;
+        if(IceboltTimer < diff)
+        {
+            DoCast(SelectUnit(SELECT_TARGET_RANDOM, 0), SPELL_ICEBOLT);
+            IceboltTimer = 11000+rand()%20000;
+        }else IceboltTimer -= diff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_boss_rage_winterchill(Creature* pCreature)
+{
+    return new boss_rage_winterchillAI(pCreature);
+}
+
+void AddSC_boss_rage_winterchill()
+{
+    Script *newscript;
+    newscript = new Script;
+    newscript->Name = "boss_rage_winterchill";
+    newscript->GetAI = &GetAI_boss_rage_winterchill;
+    newscript->RegisterSelf();
+}
---
+++ src/bindings/ScriptDev2/scripts/kalimdor/caverns_of_time/hyjal/hyjal_trash.h
@@ -0,0 +1,4 @@
+#ifndef SC_HYJAL_TRASH_AI_H
+#define SC_HYJAL_TRASH_AI_H
+
+#endif
