fd0858d914a57852686bba17994e07ac97fb10f3
unknown <Administrator@.(none)>
2011-03-27 01:51:17 +0200

    fix meta socket bug on stats

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4702,8 +4702,8 @@ void Player::SetRegularAttackTime()
 {
     for(int i = 0; i < MAX_ATTACK; ++i)
     {
-        Item *tmpitem = GetWeaponForAttack(WeaponAttackType(i),true,false);
-        if (tmpitem)
+        Item *tmpitem = GetWeaponForAttack(WeaponAttackType(i));
+        if(tmpitem && !tmpitem->IsBroken())
         {
             ItemPrototype const *proto = tmpitem->GetProto();
             if(proto->Delay)
@@ -4892,7 +4892,7 @@ void Player::UpdateWeaponSkill (WeaponAttackType attType)
     {
         case BASE_ATTACK:
         {
-            Item *tmpitem = GetWeaponForAttack(attType,true,true);
+            Item *tmpitem = GetWeaponForAttack(attType,true);
 
             if (!tmpitem)
                 UpdateSkill(SKILL_UNARMED,weapon_skill_gain);
@@ -4903,7 +4903,7 @@ void Player::UpdateWeaponSkill (WeaponAttackType attType)
         case OFF_ATTACK:
         case RANGED_ATTACK:
         {
-            Item *tmpitem = GetWeaponForAttack(attType,true,true);
+            Item *tmpitem = GetWeaponForAttack(attType,true);
             if (tmpitem)
                 UpdateSkill(tmpitem->GetSkill(),weapon_skill_gain);
             break;
@@ -6615,8 +6615,8 @@ void Player::UpdateEquipSpellsAtFormChange()
 
 void Player::CastItemCombatSpell(Unit* Target, WeaponAttackType attType)
 {
-    Item *item = GetWeaponForAttack(attType, true, true);
-    if(!item)
+    Item *item = GetWeaponForAttack(attType);
+    if(!item || item->IsBroken())
         return;
 
     ItemPrototype const *proto = item->GetProto();
@@ -6822,8 +6822,8 @@ bool Player::CheckAmmoCompatibility(const ItemPrototype *ammo_proto) const
         return false;
 
     // check ranged weapon
-    Item *weapon = GetWeaponForAttack( RANGED_ATTACK, true, false );
-    if (!weapon)
+    Item *weapon = GetWeaponForAttack( RANGED_ATTACK );
+    if(!weapon  || weapon->IsBroken() )
         return false;
 
     ItemPrototype const* weapon_proto = weapon->GetProto();
@@ -7767,14 +7767,14 @@ void Player::SetSheath( SheathState sheathed )
             break;
         case SHEATH_STATE_MELEE:                            // prepared melee weapon
         {
-            SetVirtualItemSlot(0,GetWeaponForAttack(BASE_ATTACK,true,true));
-            SetVirtualItemSlot(1,GetWeaponForAttack(OFF_ATTACK,true,true));
+            SetVirtualItemSlot(0,GetWeaponForAttack(BASE_ATTACK,true));
+            SetVirtualItemSlot(1,GetWeaponForAttack(OFF_ATTACK,true));
             SetVirtualItemSlot(2,NULL);
         };  break;
         case SHEATH_STATE_RANGED:                           // prepared ranged weapon
             SetVirtualItemSlot(0,NULL);
             SetVirtualItemSlot(1,NULL);
-            SetVirtualItemSlot(2,GetWeaponForAttack(RANGED_ATTACK,true,true));
+            SetVirtualItemSlot(2,GetWeaponForAttack(RANGED_ATTACK,true));
             break;
         default:
             SetVirtualItemSlot(0,NULL);
@@ -8144,7 +8144,7 @@ Item* Player::GetItemByPos( uint8 bag, uint8 slot ) const
     return NULL;
 }
 
-Item* Player::GetWeaponForAttack(WeaponAttackType attackType, bool nonbroken, bool useable) const
+Item* Player::GetWeaponForAttack(WeaponAttackType attackType, bool useable) const
 {
     uint16 slot;
     switch (attackType)
@@ -8159,10 +8159,10 @@ Item* Player::GetWeaponForAttack(WeaponAttackType attackType, bool nonbroken, bo
     if (!item || item->GetProto()->Class != ITEM_CLASS_WEAPON)
         return NULL;
 
-    if (useable && !IsUseEquipedWeapon(attackType==BASE_ATTACK))
-        return NULL;
+    if(!useable)
+        return item;
 
-    if (nonbroken && item->IsBroken())
+    if( item->IsBroken() || !IsUseEquipedWeapon(attackType==BASE_ATTACK) )
         return NULL;
 
     return item;
@@ -17279,7 +17279,7 @@ bool Player::EnchantmentFitsRequirements(uint32 enchantmentcondition, int8 slot)
         if(i == slot)
             continue;
         Item *pItem2 = GetItemByPos( INVENTORY_SLOT_BAG_0, i );
-        if(pItem2 && pItem2->GetProto()->Socket[0].Color)
+        if(pItem2 && !pItem2->IsBroken() && pItem2->GetProto()->Socket[0].Color)
         {
             for(uint32 enchant_slot = SOCK_ENCHANTMENT_SLOT; enchant_slot < SOCK_ENCHANTMENT_SLOT+3; ++enchant_slot)
             {
@@ -18595,7 +18595,7 @@ bool Player::IsAtGroupRewardDistance(WorldObject const* pRewardSource) const
 
 uint32 Player::GetBaseWeaponSkillValue (WeaponAttackType attType) const
 {
-    Item* item = GetWeaponForAttack(attType,true,true);
+    Item* item = GetWeaponForAttack(attType,true);
 
     // unarmed only with base attack
     if(attType != BASE_ATTACK && !item)
