220d3e02dea630ec63ef7f52c7f037129ab473f3
unknown <Admin@.(none)>
2011-04-18 10:26:39 +0300

    WPE/Dupe auto-ban

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -15509,6 +15509,7 @@ void Player::_SaveInventory()
     {
         sLog.outError("Player::_SaveInventory - one or more errors occurred save aborted!");
         ChatHandler(this).SendSysMessage(LANG_ITEM_SAVE_FAILED);
+        sWorld.BanAccount(BAN_CHARACTER, GetName(), "-1", "WPE/Dupe", "auto-ban");
         return;
     }
 
